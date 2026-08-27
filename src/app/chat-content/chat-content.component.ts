616f357c02dbfa27bb4030be7cf9bd48ea8ccbe9
Adrian55Stack <adrianciurea112@yahoo.com>
2026-08-27 23:52:08 +0300

    Extract message component

--- src/app/chat-content/chat-content.component.ts
+++ src/app/chat-content/chat-content.component.ts
@@ -2,17 +2,15 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { IMessage } from '../models/message.model';
 import { Observable } from 'rxjs';
-import { AsyncPipe, DatePipe } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { CoreMessageService } from '../services/core-message.service';
-import { marked } from 'marked';
-import DOMPurify from 'dompurify';
 import { TypingInProgressComponent } from "../typing-in-progress/typing-in-progress.component";
-import { AvatarComponent } from '../avatar/avatar.component';
 import { EmptyListComponent } from "../empty-list/empty-list.component";
+import { MessageComponent } from "../message/message.component";
 
 @Component({
   selector: 'app-chat-content',
-  imports: [MatExpansionModule, AsyncPipe, DatePipe, TypingInProgressComponent, AvatarComponent, EmptyListComponent],
+  imports: [MatExpansionModule, AsyncPipe, TypingInProgressComponent, EmptyListComponent, MessageComponent],
   templateUrl: './chat-content.component.html',
   styleUrl: './chat-content.component.scss'
 })
@@ -25,10 +23,5 @@ export class ChatContentComponent implements OnInit {
   ngOnInit(): void {
     this.messages = this.coreMessageService.getMessages();
     this.botIsTyping = this.coreMessageService.getInProgress();
-  }
-
-  parseMarkdown(msg: string): string {
-      const html = (marked(msg) as string).replace('<p','<p class="mb-0"');
-      return DOMPurify.sanitize(html);
   }
 }
