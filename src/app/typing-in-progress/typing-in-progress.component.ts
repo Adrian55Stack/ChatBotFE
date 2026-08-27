92c3e8afbf2cbadc13d57b07890277ae2c022405
Adrian55Stack <adrianciurea112@yahoo.com>
2026-08-27 18:00:18 +0300

    Add file

---
+++ src/app/typing-in-progress/typing-in-progress.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-in-progress',
+  imports: [],
+  templateUrl: './typing-in-progress.component.html',
+  styleUrl: './typing-in-progress.component.scss'
+})
+export class TypingInProgressComponent {
+
+}
