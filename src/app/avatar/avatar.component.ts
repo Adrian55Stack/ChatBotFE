0d63abfd9d0cc10f76542184c44f7b395810be04
Adrian55Stack <adrianciurea112@yahoo.com>
2026-08-29 13:35:00 +0300

    Add puny code to test sonar coverage

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -9,4 +9,14 @@ import { Component, input } from '@angular/core';
 export class AvatarComponent {
   size = input.required<number>();
   altText = 'seshat';
+
+  showMessage1(){
+    console.log(1);
+  }
+  showMessage2(){
+    console.log(1);
+  }
+  showMessage3(){
+    console.log(1);
+  }
 }
