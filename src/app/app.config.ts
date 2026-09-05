d8abb9bee468f3dc0029d57639b3a3f094907cef
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-05 17:09:22 +0300

    Add basic use of translation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideHttpClient(),
+     provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './i18n/',
+        suffix: '.json',
+      }),
+    }),
+  ]
 };
--- src/app/chat-header/chat-header.component.ts
+++ src/app/chat-header/chat-header.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-chat-header',
   templateUrl: './chat-header.component.html',
-  styleUrl: './chat-header.component.scss'
+  styleUrl: './chat-header.component.scss',
+  imports: [TranslatePipe]
 })
 export class ChatHeaderComponent {
+  private readonly translate: TranslateService = inject(TranslateService);
   appTitle = 'SeshatAI';
   subtitle = 'Ancient stories. Modern answers.';
+  constructor() {
+    this.translate.use('zh');
+  }
 }
