1a40f067f1a22617b6da3d99b272e95c49522b3f
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-06 22:05:27 +0300

    Add basic drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { ChatHeaderComponent } from './chat-header/chat-header.component';
 import { ChatFooterComponent } from './chat-footer/chat-footer.component';
 import { ChatContentComponent } from './chat-content/chat-content.component';
 import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 @Component({
   selector: 'app-root',
-  imports: [ChatHeaderComponent, ChatContentComponent, ChatFooterComponent, MatCardModule],
+  imports: [ChatHeaderComponent, ChatContentComponent, ChatFooterComponent, MatCardModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
