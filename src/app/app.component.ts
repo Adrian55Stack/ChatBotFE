18abcc67e895ba277b49f08cdf6c719ebd4a220e
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-06 22:46:32 +0300

    Adapt sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { ConversationComponent } from "./conversation/conversation.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [ConversationComponent, MatCardModule, MatSidenavModule, ConversationComponent],
+  imports: [ConversationComponent, MatCardModule, MatSidenavModule, ConversationComponent, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
