4093819d9b7d47aeb956d7e27e70001911776cc4
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-11 00:32:47 +0300

    Add language selector component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import { ConversationComponent } from "./conversation/conversation.component";
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { LanguageSelectorComponent } from "./components/language-selector/language-selector.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ConversationComponent, MatCardModule, MatSidenavModule, ConversationComponent, MatIconModule, MatButtonModule],
+  imports: [ConversationComponent, MatCardModule, MatSidenavModule, ConversationComponent, MatIconModule, MatButtonModule, LanguageSelectorComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/components/language-selector/language-selector.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-language-selector',
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule],
+  templateUrl: './language-selector.component.html',
+  styleUrl: './language-selector.component.scss'
+})
+export class LanguageSelectorComponent {
+
+  languages = [
+    { id: 1, src: 'flags/en.jpg', label: 'English', value: 'en' },
+    { id: 2, src: 'flags/fr.jpg', label: 'French', value: 'fr' },
+    { id: 3, src: 'flags/zh.jpg', label: 'Chinese', value: 'zh' },
+    { id: 4, src: 'flags/es.jpg', label: 'Spanish', value: 'es' },
+    { id: 5, src: 'flags/ar.jpg', label: 'Arabic', value: 'ar' },
+    { id: 6, src: 'flags/ru.jpg', label: 'Russian', value: 'ru' },
+    { id: 7, src: 'flags/ja.jpg', label: 'Japanese', value: 'ja' },
+    { id: 8, src: 'flags/it.jpg', label: 'Italian', value: 'it' },
+    { id: 9, src: 'flags/pl.jpg', label: 'Polish', value: 'pl' },
+    { id: 10, src: 'flags/ro.jpg', label: 'Romanian', value: 'ro' },
+  ];
+
+  selectedLanguage = this.languages[0];
+
+}
