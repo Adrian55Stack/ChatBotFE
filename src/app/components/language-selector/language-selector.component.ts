ca0a9dc7902b96f11a0cd719800ae52f2755de35
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-11 23:33:47 +0300

    Add translations

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,33 +1,40 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { FlagIconComponent } from "../flag-icon/flag-icon.component";
-import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language-selector',
-  imports: [MatFormFieldModule, MatSelectModule, FormsModule, FlagIconComponent],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, FlagIconComponent, TranslatePipe],
   templateUrl: './language-selector.component.html',
   styleUrl: './language-selector.component.scss'
 })
-export class LanguageSelectorComponent {
+export class LanguageSelectorComponent implements OnInit {
   private readonly translateService = inject(TranslateService);
 
+  flagsPath = 'flags/';
+  flagsFormat = '.jpg';
+
   languages = [
-    { id: 1, src: 'flags/en.jpg', label: 'English', value: 'en' },
-    { id: 2, src: 'flags/fr.jpg', label: 'French', value: 'fr' },
-    { id: 3, src: 'flags/zh.jpg', label: 'Chinese', value: 'zh' },
-    { id: 4, src: 'flags/es.jpg', label: 'Spanish', value: 'es' },
-    { id: 5, src: 'flags/ar.jpg', label: 'Arabic', value: 'ar' },
-    { id: 6, src: 'flags/ru.jpg', label: 'Russian', value: 'ru' },
-    { id: 7, src: 'flags/ja.jpg', label: 'Japanese', value: 'ja' },
-    { id: 8, src: 'flags/it.jpg', label: 'Italian', value: 'it' },
-    { id: 9, src: 'flags/pl.jpg', label: 'Polish', value: 'pl' },
-    { id: 10, src: 'flags/ro.jpg', label: 'Romanian', value: 'ro' },
+    { id: 1, src: `${this.flagsPath}en${this.flagsFormat}`, label: 'sidEnglish', value: 'en' },
+    { id: 2, src: `${this.flagsPath}fr${this.flagsFormat}`, label: 'sidFrench', value: 'fr' },
+    { id: 3, src: `${this.flagsPath}zh${this.flagsFormat}`, label: 'sidChinese', value: 'zh' },
+    { id: 4, src: `${this.flagsPath}es${this.flagsFormat}`, label: 'sidSpanish', value: 'es' },
+    { id: 5, src: `${this.flagsPath}ar${this.flagsFormat}`, label: 'sidArabic', value: 'ar' },
+    { id: 6, src: `${this.flagsPath}ru${this.flagsFormat}`, label: 'sidRussian', value: 'ru' },
+    { id: 7, src: `${this.flagsPath}ja${this.flagsFormat}`, label: 'sidJapanese', value: 'ja' },
+    { id: 8, src: `${this.flagsPath}it${this.flagsFormat}`, label: 'sidItalian', value: 'it' },
+    { id: 9, src: `${this.flagsPath}pl${this.flagsFormat}`, label: 'sidPolish', value: 'pl' },
+    { id: 10, src: `${this.flagsPath}ro${this.flagsFormat}`, label: 'sidRomanian', value: 'ro' }
   ];
 
-  selectedLanguage = this.languages[0];
+  selectedLanguage;
+
+  ngOnInit(): void {
+   this.selectedLanguage = this.languages.find(l => l.value === 'en'); 
+  }
 
   updateLanguage($event: MatSelectChange): void {
     this.translateService.use($event.value.value);
