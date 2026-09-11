e7eacc898d250ff00982a7b775f5b1743103cc05
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-11 23:55:38 +0300

    add manifest, fetch languages async

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { FlagIconComponent } from "../flag-icon/flag-icon.component";
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { Language } from '../../models/language.model';
+import { LanguageService } from '../../services/language.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-language-selector',
@@ -14,27 +16,18 @@ import { Language } from '../../models/language.model';
 })
 export class LanguageSelectorComponent implements OnInit {
   private readonly translateService = inject(TranslateService);
+  private readonly languageService = inject(LanguageService);
+  private readonly destroyRef = inject(DestroyRef);
 
-  flagsPath = 'flags/';
-  flagsFormat = '.jpg';
-
-  languages: Language[] = [
-    { id: 1, src: `${this.flagsPath}en${this.flagsFormat}`, label: 'sidEnglish', value: 'en' },
-    { id: 2, src: `${this.flagsPath}fr${this.flagsFormat}`, label: 'sidFrench', value: 'fr' },
-    { id: 3, src: `${this.flagsPath}zh${this.flagsFormat}`, label: 'sidChinese', value: 'zh' },
-    { id: 4, src: `${this.flagsPath}es${this.flagsFormat}`, label: 'sidSpanish', value: 'es' },
-    { id: 5, src: `${this.flagsPath}ar${this.flagsFormat}`, label: 'sidArabic', value: 'ar' },
-    { id: 6, src: `${this.flagsPath}ru${this.flagsFormat}`, label: 'sidRussian', value: 'ru' },
-    { id: 7, src: `${this.flagsPath}ja${this.flagsFormat}`, label: 'sidJapanese', value: 'ja' },
-    { id: 8, src: `${this.flagsPath}it${this.flagsFormat}`, label: 'sidItalian', value: 'it' },
-    { id: 9, src: `${this.flagsPath}pl${this.flagsFormat}`, label: 'sidPolish', value: 'pl' },
-    { id: 10, src: `${this.flagsPath}ro${this.flagsFormat}`, label: 'sidRomanian', value: 'ro' }
-  ];
+  languages: Language[];
 
   selectedLanguage: Language;
 
   ngOnInit(): void {
-   this.selectedLanguage = this.languages.find(l => l.value === 'en'); 
+    this.languageService.getLanguages().pipe(takeUntilDestroyed(this.destroyRef)).subscribe((langs) => {
+      this.languages = langs;
+      this.selectedLanguage = this.languages.find(l => l.value === 'en'); 
+  });
   }
 
   updateLanguage($event: MatSelectChange): void {
