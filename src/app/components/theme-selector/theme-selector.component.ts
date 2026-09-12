2b7ced45c17da4dfe75188f555cde15dde66e534
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-12 22:25:08 +0300

    Add translations

--- src/app/components/theme-selector/theme-selector.component.ts
+++ src/app/components/theme-selector/theme-selector.component.ts
@@ -3,10 +3,11 @@ import { ThemeService } from '../../services/theme.service';
 import {MatButtonToggleChange, MatButtonToggleModule} from '@angular/material/button-toggle';
 import { Theme } from '../../models/theme.model';
 import { Mythology } from '../../constants/mythology.constant';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-theme-selector',
-  imports: [MatButtonToggleModule],
+  imports: [MatButtonToggleModule, TranslatePipe],
   templateUrl: './theme-selector.component.html',
   styleUrl: './theme-selector.component.scss'
 })
@@ -15,9 +16,9 @@ export class ThemeSelectorComponent {
   currentTheme = this.themeService.theme;
 
   themes: Theme[] = [
-    { value: Mythology.Egypt, label: 'Egyptian' },
-    { value: Mythology.Norse, label: 'Norse'},
-    { value: Mythology.Greek, label: 'Greek'}];
+    { value: Mythology.Egypt, label: 'sidEgyptianTheme' },
+    { value: Mythology.Norse, label: 'sidNorseTheme'},
+    { value: Mythology.Greek, label: 'sidGreekTheme'}];
 
   selectTheme(event: MatButtonToggleChange) {
     this.themeService.setTheme(event.value);
