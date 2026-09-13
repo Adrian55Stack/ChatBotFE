fcf4f3b3a2deb36e43e45427a613889585e399e9
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-13 13:24:35 +0300

    Fix tests

--- src/app/components/theme-selector/theme-selector.component.spec.ts
+++ src/app/components/theme-selector/theme-selector.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ThemeSelectorComponent } from './theme-selector.component';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { FakeTranslateLoader } from '../../mocks/fake-translate-loader';
 
 describe('ThemeSelectorComponent', () => {
   let component: ThemeSelectorComponent;
@@ -8,7 +10,10 @@ describe('ThemeSelectorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ThemeSelectorComponent]
+      imports: [ThemeSelectorComponent],
+      providers: [provideTranslateService({
+        loader: { provide: TranslateLoader, useClass: FakeTranslateLoader },
+      })]
     })
     .compileComponents();
 
