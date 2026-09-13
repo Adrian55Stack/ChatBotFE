169847547451860d24698a6eb6832e3b40dca0ea
Adrian55Stack <adrianciurea112@yahoo.com>
2026-09-13 13:06:06 +0300

    Update font

--- src/app/components/theme-selector/theme-selector.component.ts
+++ src/app/components/theme-selector/theme-selector.component.ts
@@ -15,13 +15,15 @@ import { FormsModule } from '@angular/forms';
 })
 export class ThemeSelectorComponent {
   private readonly themeService = inject(ThemeService);
+  private readonly symbolsPath = 'theme-symbols/';
+  private readonly format = '.png';
   currentTheme = this.themeService.theme;
   size = 40;
 
   themes: Theme[] = [
-    { value: Mythology.Egypt, label: 'sidEgyptianTheme', source: 'theme-symbols/egypt.png' },
-    { value: Mythology.Norse, label: 'sidNorseTheme', source: 'theme-symbols/norse.png'},
-    { value: Mythology.Greek, label: 'sidGreekTheme', source: 'theme-symbols/greek.png'}];
+    { value: Mythology.Egypt, label: 'sidEgyptianTheme', source: `${this.symbolsPath}egypt${this.format}` },
+    { value: Mythology.Norse, label: 'sidNorseTheme', source: `${this.symbolsPath}norse${this.format}`},
+    { value: Mythology.Greek, label: 'sidGreekTheme', source: `${this.symbolsPath}greek${this.format}`}];
 
   selectTheme(event: MatButtonToggleChange) {
     this.themeService.setTheme(event.value);
